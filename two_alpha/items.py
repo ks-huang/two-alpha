a927584e522aa65e1533061199c97b4e4ffb54b4
KS Huang <kshuan9@gmail.com>
2020-11-22 16:49:27 -0500

    place holder for diff generator

--- two_alpha/items.py
+++ two_alpha/items.py
@@ -4,9 +4,9 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from itemloaders.processors import TakeFirst
 
 class ReloadingItem(scrapy.Item):
     # define the fields for your item here like:
-    product_name = scrapy.Field()
-    url = scrapy.Field()
+    product_name = scrapy.Field(output_processor=TakeFirst())
+    url = scrapy.Field(output_processor=TakeFirst())
--- two_alpha/pipelines.py
+++ two_alpha/pipelines.py
@@ -6,8 +6,38 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import json
+import logging
 
 class TwoAlphaPipeline:
+    snapshot = {}
+
+    def open_spider(self, spider):
+        try:
+            with open('snapshot.json', 'r') as f:
+                self.lastSnapshot = json.load(f)
+        except IOError:
+            self.lastSnapshot = {}
+
+    def close_spider(self, spider):
+        # dump product added
+        diff = { k : self.snapshot[k] for k, _ in set(self.snapshot.items()) - set(self.lastSnapshot.items()) }
+        logging.info("Newly added...")
+        logging.info(diff)
+
+        # dump product removed
+        diff = { k : self.lastSnapshot[k] for k in set(self.lastSnapshot) - set(self.snapshot) }
+        logging.info("Just gone...")
+        logging.info(diff)
+
+        # update snapshot
+        data = json.dumps(self.snapshot)
+        with open('snapshot.json', 'w') as outfile:
+            outfile.write(data)
+
     def process_item(self, item, spider):
+        prod = item['product_name']
+        url = item['url']
+
+        self.snapshot[prod] = url
         return item
