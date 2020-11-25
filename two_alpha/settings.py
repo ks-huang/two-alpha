b8fa198e8c2dc9e99ee8ed4c02fbbcff5f3d25aa
ks-huang <kshuan9@gmail.com>
2020-11-25 07:59:22 +0000

    Enable user-agent shuffling

--- two_alpha/settings.py
+++ two_alpha/settings.py
@@ -15,7 +15,42 @@ NEWSPIDER_MODULE = 'two_alpha.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'two_alpha (+http://www.yourdomain.com)'
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
+# USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E148 Safari/604.1'
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/86.0.4240.193 Safari/537.36'), # chrome
+    ('Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) '
+     'AppleWebKit/605.1.15 (KHTML, like Gecko) '
+     'Version/14.0.1 Mobile/15E148 Safari/604.1'), # safari
+    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) '
+     'AppleWebKit/605.1.15 (KHTML, like Gecko) '
+     'Version/14.0.1 Safari/605.1.15'), # safari
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0'),  # firefox
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.91 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/62.0.3202.89 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/63.0.3239.108 '
+     'Safari/537.36'),  # chrome
+]
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -32,7 +67,7 @@ DOWNLOAD_DELAY = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -54,6 +89,10 @@ COOKIES_ENABLED = False
 #DOWNLOADER_MIDDLEWARES = {
 #    'two_alpha.middlewares.TwoAlphaDownloaderMiddleware': 543,
 #}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'two_alpha.useragents.UserAgentsMiddleware': 500,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- two_alpha/spiders/powder_valley.py
+++ two_alpha/spiders/powder_valley.py
@@ -5,26 +5,27 @@ from two_alpha.items import ReloadingItem
 
 class PowderValleySpider(scrapy.Spider):
     name = 'PowderValley'
-    args = [
-        [125, 180, 220, random.randint(100,250)],
-        ['cci', 'federal-reloading-supplies', 'cc' + chr(random.randint(ord('a'), ord('z')))],
-        ['300-aac-blackout','308-win', '{}-win'.format(random.randint(301, 999))]
-    ]
-
-    start_urls = [
+    preproc_urls = {
         ('https://www.powdervalleyinc.com/product-category/reloading-supplies/bullets/rifle/'
-         '?query_type_grain-weight=or&filter_grain-weight={}-gr%2C{}-gr%2C{}-gr%2C{}-gr&'
-         'filter_caliber-range-sort-by=307-309&query_type_caliber-range-sort-by=or&filter_brand=sierra&query_type_brand=or'),
+         '?query_type_grain-weight=or&filter_grain-weight={}-gr%2C{}-gr%2C{}-gr%2C{}-gr%2C{}-gr&'
+         'filter_caliber-range-sort-by=307-309&query_type_caliber-range-sort-by=or&filter_brand=sierra&query_type_brand=or') :
+        [125, 180, 168, 220, random.randint(231,250)],
+
         ('https://www.powdervalleyinc.com/product-category/reloading-supplies/primers/?query_type_product-type=or'
-         '&filter_brand={}%2C{}%2C{}&query_type_brand=or&filter_product-type=small-rifle-primers'),
+         '&filter_brand={}%2C{}%2C{}&query_type_brand=or&filter_product-type=small-rifle-primers') :
+        ['cci', 'federal-reloading-supplies', 'cc' + chr(random.randint(ord('a'), ord('z')))],
+
         ('https://www.powdervalleyinc.com/product-category/reloading-supplies'
-         '/brass/rifle-brass/?filter_caliber-range-sort-by={}%2C{}%2C{}&query_type_caliber-range-sort-by=or'),
-    ]
+         '/brass/rifle-brass/?filter_caliber-range-sort-by={}%2C{}%2C{}&query_type_caliber-range-sort-by=or') :
+        ['300-aac-blackout','308-win', '{}-win'.format(random.randint(301, 999))]
+    }
+
+    start_urls = []
 
     def __init__(self):
-        for ix in range(len(self.start_urls)):
-            random.shuffle(self.args[ix])
-            self.start_urls[ix] = self.start_urls[ix].format(*self.args[ix])
+        for url, args in self.preproc_urls.items():
+            random.shuffle(args)
+            self.start_urls.append(url.format(*args))
 
         super().__init__
 
